1a2f65ab898b2f5c314a822df72440dd21295a3a
shincyls <shincy.mac@gmail.com>
2019-09-26 00:05:20 +0800

    message

--- app/controllers/poll_candidates_controller.rb
+++ app/controllers/poll_candidates_controller.rb
@@ -5,9 +5,10 @@ class PollCandidatesController < ApplicationController
 
     private
 
-    unless current_user.super?
-        flash.now[:warning] = "Super User required to peform this action."
+    def
+        unless current_user.super?
+            flash.now[:warning] = "Super User required to peform this action."
+        end
     end
-end
     
 end
