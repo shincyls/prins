3ca36b50c9e2c3dd82675b99b9721181119352e6
shincyls <shincy.mac@gmail.com>
2019-09-25 23:49:58 +0800

    update

--- db/migrate/20190902000000_create_registers.rb
+++ db/migrate/20190902000000_create_registers.rb
@@ -3,11 +3,6 @@ class CreateRegisters < ActiveRecord::Migration[5.2]
     create_table :registers do |t|
       t.references :event, default: 1
       t.string :full_name
-      t.string :first_name
-      t.string :last_name
-      t.string :phone_number
-      t.string :phone_number_2
-      t.string :identity_number
       t.float :purchase_amount, default: 0
       t.integer :drawing_chance, default: 0
       t.string :ticket_number, limit: 3
@@ -19,11 +14,6 @@ class CreateRegisters < ActiveRecord::Migration[5.2]
       t.boolean :draw_allowed, default: true
       t.string :company
       t.string :department
-      t.string :info_1
-      t.string :info_2
-      t.string :info_3
-      t.string :info_4
-      t.string :info_5
       t.timestamps
     end
   end
