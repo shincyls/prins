309cc2fb51cc09ff6ac40822955b7de62ab887ed
shincyls <shincy.mac@gmail.com>
2019-09-26 19:32:29 +0800

    update

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,7 +1,7 @@
 class RegistersController < ApplicationController
   include ApplicationHelper
   
-  before_action :set_register, only: [:show, :edit, :update, :destroy]
+  before_action :set_register, only: [:show, :attendance, :checkin, :printa, :printupdate, :edit, :update, :destroy]
   before_action :require_super, only: [:destroy]
   before_action :require_login
 
@@ -18,55 +18,43 @@ class RegistersController < ApplicationController
   def list
     respond_to :html, :js
     @data = params[:button]
-    
     if @data == "3"
       @registers = Register.all
     else
       @registers = Register.all.where(category: @data)
     end
-
     if params[:check]
       @registers = @registers.where(checkin: false)
     end
-
     if params[:uncheck]
       @registers = @registers.where(checkin: true)
     end
-
     if params[:hide_sort]
       @registers = @registers.where(status: 0)
     end
-
     if params[:ticket_sort]
       @registers = @registers.order("ticket_number desc NULLS LAST")
     end
-
     @registers = @registers.order("created_at desc")
-
   end
 
   def show
-    @register = Register.find(params[:id])
   end
 
   def attendance
     respond_to :html, :js
-    @register = Register.find(params[:id])
     @register.attendance = !@register.attendance
     @register.save
   end
 
   def checkin
     respond_to :html, :js
-    @register = Register.find(params[:id])
     @register.checkin = !@register.checkin
     @register.save
   end
 
   def printa
     respond_to :html, :js
-    @register = Register.find(params[:id])
-
     if @register.ticket_number.nil? && @register.draw_allowed
       @register.convert_ticket_number
       @register.save
@@ -83,7 +71,6 @@ class RegistersController < ApplicationController
 
   def printupdate
     respond_to :html, :js
-    @register = Register.find(params[:id])
     @register.status = 1
     @register.save
   end
@@ -96,15 +83,12 @@ class RegistersController < ApplicationController
       r.checkin = false
       r.save
     end
-
     @content = PageContent.find_by(name: "running_number")
     @content.value = 1
     @content.save
-    
     respond_to do |format|
       format.html { redirect_to registers_path, notice: "Ticket Number and Printing Status has been successfully reset." }
     end
-
   end
 
   # GET /registers/new
@@ -120,7 +104,6 @@ class RegistersController < ApplicationController
   def create
     respond_to do |format|
       @register = Register.new(register_params)
-
       if @register.save
         format.html { redirect_to new_register_path, notice: "#{@register.full_name} from #{@register.info_1} with table #{@register.info_2} has been successfully registered." }
       else
@@ -165,15 +148,15 @@ class RegistersController < ApplicationController
       end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def register_params
-      params.require(:register).permit(:full_name, :first_name, :last_name, :phone_number, :phone_number_2, :identity_number, :category, :drawing_chance, :info_1, :info_2, :info_3, :info_4, :info_5, :attendance, :company, :department, :employee_id, :draw_allowed, :event_id)
-    end
-
     def require_super
       unless logged_in? & current_user.super?
           flash.now[:warning] = "Super User required to peform this action."
       end
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def register_params
+      params.require(:register).permit(:full_name, :first_name, :last_name, :phone_number, :phone_number_2, :identity_number, :category, :drawing_chance, :info_1, :info_2, :info_3, :info_4, :info_5, :attendance, :company, :department, :employee_id, :draw_allowed, :event_id)
     end
 
 end
